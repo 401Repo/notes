7a7717aeb4f68e48926a3106079cf4fd4b3c0ef9
thatvetdevrob <thatvetdevrob@gmail.com>
2020-10-15 22:40:33 -0400

    finished up lab 3 now need uml and turning it in

--- index.js
+++ index.js
@@ -1,39 +1,29 @@
 'use strict';
-console.log('#-----------------------------#');
-console.log('#---------- Start ------------#');
+
 console.log('                               ');
+
+// lets make instructions:
+
+console.log('oooo   oooo  ooooooo   ooooooooooo ooooooooooo  oooooooo8 ooooo  oooo ');
+console.log(' 8888o  88 o888   888o 88  888  88  888    88  888          888  88   ');
+console.log(' 88 888o88 888     888     888      888ooo8     888oooooo     888     ');
+console.log(' 88   8888 888o   o888     888      888    oo          888    888     ');
+console.log('o88o    88   88ooo88      o888o    o888ooo8888 o88oooo888    o888o    ');
+console.log('');
+console.log('');
+
 
 // Get ENV info
 require('dotenv').config();
 
 // loading my modules
+const HTTP = require('./lib/http.js');
 const Input = require('./lib/input.js');
 const Note = require('./lib/notes.js');
+const NoteModel = require('./lib/models / requests/noteModel');
 
-// Connect to the db
 const mongoose = require('mongoose');
-
-// connect to atlas
-
-mongoose.connect(process.env.mongoLink, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true }
-)
-  .then( () => console.log('connected to mongo'));
-
-const { Schema } = mongoose;
-
-// lets create a mongoose (model) scheema
-
-const NoteSchema = new Schema({
-  text:{type:String, required: true},
-  added:{ type:Boolean, default:false},
-  category:{type:String, required:false},
-});
-
-const NoteModel = mongoose.model('Notes', NoteSchema);
-
-// now lets run our app
+const noteModel = require('./lib/models / requests/noteModel');
 
 // fist thing to do is to pass on the info of the argsv to a variable
 
@@ -48,71 +38,69 @@ if (workingNote.valid() === true) {
   object.execute();
 
 
-  // what rudimentary info do i need to persist an objec?
-  // console.log(` this is the object: ${object}`);
-  // console.log(` this is the object text: ${object.payload}`);
-  // console.log(` this is the object category: ${object.action}`);
+  // send to mongoose if action is add
 
-  const newNote = new NoteModel({
-    text: object.payload,
-    category: 'default',
-    added: true,
-  });
+  if(object.action === '--add' || object.action === '-a'){
 
-  // send it over to the db
-  newNote.save();
+    mongoose.connect(process.env.mongoLink, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true }
+    ).then(()=> {
 
-  console.log('                               ');
-  console.log('#---------- End --------------#');
-  console.log('#-----------------------------#');
-} else {
-  console.log('                               ');
-  console.log('#---------- End --------------#');
-  console.log('#-----------------------------#');
+      const newNote = new NoteModel({
+        text: object.payload,
+        category: 'default',
+        added: true,
+      });
+      newNote.save();
+
+    });
+
+    console.log('                               ');
+    console.log('#---------- End --------------#');
+    console.log('#-----------------------------#');
+  } else if(object.action === '--list' || object.action === '-l'){
+
+
+    console.log('-------Your Notes Bellow-------');
+    console.log('#-----------------------------#');
+
+
+    mongoose.connect(process.env.mongoLink, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true }
+    )
+    noteModel.find({})
+      .then(notes => console.log(notes))
+      .then( () => console.log('#-----------------------------#'))
+      .then( () => console.log('#------------End--------------#'));
+
+  }
+  else if(object.action === '--delete' || object.action === '-d'){
+
+
+    console.log('-------   Deleting...  -------');
+    console.log('#-----------------------------#');
+
+
+    mongoose.connect(process.env.mongoLink, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true }
+    )
+
+    noteModel.deleteOne({ text: `${object.payload}`}, function (err) {
+      if(err) console.log('Sorry, not found.');
+      console.log('Gone!');
+    });
+
+
+  }
+  else {
+    console.log('                               ');
+    console.log('#---------- End --------------#');
+    console.log('#-----------------------------#');
+  }
+
 }
 
 
-
-// Search our notes BUT delay so it populate to mongo
-
-setTimeout(function(){
-  console.log('#-----------------------------#');
-  console.log('#-----------------------------#');
-  console.log();
-  console.log('These are our saved notes:');
-  console.log('#-----------------------------#');
-  console.log('#-----------------------------#');
-  NoteModel.find({})
-    .then(notes => console.log(notes));
-}, 5000);
-
-
-
-
-
-
-
-
-// // old code
-
-// const Input = require('./lib/input.js');
-// const Notes = require('./lib/notes.js');
-
-// const minimist = require('minimist');
-
-
-// // This slices off the first two indexes and is the method for minimist
-// let args = minimist(process.argv.slice(2));
-
-// console.log(args);
-// //args is an object that holds whatever command and arguments are passed in
-// const input = new Input(args);
-
-// //if input action is valid, then create new note
-// //note has an id and a payload which will be added as text
-
-// if(input.valid()) {
-//   new Notes(input);
-// } else {
-//   throw new Error('invalid action');
-// }
