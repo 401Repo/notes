c294ef84ca47b446ae9ed07fb20fa346cc28f9b6
thatvetdevrob <thatvetdevrob@gmail.com>
2020-10-18 18:58:31 -0400

    All crud operations ready

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 'use strict';
 
-console.log('                               ');
-
-// lets make instructions:
-
+//>o) < Banner *QUACK*
+//(_>')
+console.log();
 console.log('oooo   oooo  ooooooo   ooooooooooo ooooooooooo  oooooooo8 ooooo  oooo ');
 console.log(' 8888o  88 o888   888o 88  888  88  888    88  888          888  88   ');
 console.log(' 88 888o88 888     888     888      888ooo8     888oooooo     888     ');
@@ -13,34 +12,40 @@ console.log('');
 console.log('');
 
 
-// Get ENV info
+//>o) < secret env info *QUACK*
+//(_>')
 require('dotenv').config();
 
-// loading my modules
-const HTTP = require('./lib/http.js');
+//>o) < load modules *QUACK*
+//(_>')
+
+// const HTTP = require('./lib/http.js');
 const Input = require('./lib/input.js');
 const Note = require('./lib/notes.js');
 const NoteModel = require('./lib/models / requests/noteModel');
-
 const mongoose = require('mongoose');
 const noteModel = require('./lib/models / requests/noteModel');
 
-// fist thing to do is to pass on the info of the argsv to a variable
+//>o) < read command line *QUACK*
+//(_>')
 
 let info = process.argv;
 
-//take the variable and then pass it as an iput to the constructor
+//>o) < validate info *QUACK*
+//(_>')
 
-let workingNote = new Input(info);
+let noteinfo = new Input(info);
 
-if (workingNote.valid() === true) {
-  let object = new Note(workingNote);
-  object.execute();
+//>o) < If valid lets start my if statements *QUACK*
+//(_>')
 
+if (noteinfo.valid() === true) {
+  let data = new Note(noteinfo);
+  data.execute();
 
   // send to mongoose if action is add
 
-  if(object.action === '--add' || object.action === '-a'){
+  if(data.action === '--add' || data.action === '-a'){
 
     mongoose.connect(process.env.mongoLink, {
       useNewUrlParser: true,
@@ -48,21 +53,41 @@ if (workingNote.valid() === true) {
     ).then(()=> {
 
       const newNote = new NoteModel({
-        text: object.payload,
-        category: 'default',
+        text: data.payload,
+        category: data.category || 'Default',
         added: true,
       });
-      newNote.save();
+      newNote.save().then(()=> {
+        process.exit(1);
+        //>o) < program will exit after promise saves to mongodb *QUACK*
+        //(_>')
+      });
+      //
+      //TODO: USE COLLECTIONS
+      //
+      //HTTP.fetch(newNote);
+      // }).then( () => {
 
+    //   console.log('Trying to land here... saving my notes again using collections method');
+    //   const request = new HTTP({text:data.text, category:data.category, added:true});
+    //   return request.fetch()
     });
 
     console.log('                               ');
     console.log('#---------- End --------------#');
     console.log('#-----------------------------#');
-  } else if(object.action === '--list' || object.action === '-l'){
+    console.log();
+    //>o) < will print before saving to mongo but not before its saved *QUACK*
+    //(_>')
+
+  } else if(data.action === '--list' || data.action === '-l'){
 
 
-    console.log('-------Your Notes Bellow-------');
+    //>o) < List either a catgory, all, or say none found *QUACK*
+    //(_>')
+
+    console.log();
+    console.log('-----Listing Notes Bellow------');
     console.log('#-----------------------------#');
 
 
@@ -70,14 +95,56 @@ if (workingNote.valid() === true) {
       useNewUrlParser: true,
       useUnifiedTopology: true }
     )
-    noteModel.find({})
-      .then(notes => console.log(notes))
-      .then( () => console.log('#-----------------------------#'))
-      .then( () => console.log('#------------End--------------#'));
+    //>o) < say command is: node index.js -l 'Birds' = Birds is the category searched
+    //(_>')
 
+    if(data.payload !== undefined){
+      console.log(`Notes under ${data.payload}: `);
+      console.log();
+      noteModel.find({category:data.payload}, function(err, results){
+        if(err){console.log('Error')}
+
+        //>o) < if thre is an input but none found then throw error message and exit
+        //(_>')
+
+        if(!results.length) {
+          console.log('No notes under that category found');
+          process.exit(1);
+        }
+      })
+        .then(notes => console.log(notes))
+        .then( () => console.log('#-----------------------------#'))
+        .then( () => console.log('#------------End--------------#'))
+        .then(() => {console.log();
+          process.exit(1)})
+
+    } else {
+      console.log('Notes under every category: ');
+      console.log();
+      noteModel.find({})
+        .then(notes => console.log(notes))
+        .then( () => console.log('#-----------------------------#'))
+        .then( () => console.log('#------------End--------------#'))
+        .then(() => {console.log();
+          process.exit(1)})
+    }
+
+    //TODO USE COLLECTIONS
+    // .then( () => {
+
+    //   console.log('Trying to land here... List my notes again using collections method');
+    //   const request = new HTTP();
+    //   return request.list().then(
+    //     requests => console.log(requests))
+    // });
   }
-  else if(object.action === '--delete' || object.action === '-d'){
+  else if(data.action === '--delete' || data.action === '-d'){
 
+    //>o) < Instead of an ID we can delete by body of a note:
+    //(_>') Say you got category 'Animals' and want to delete
+    //      'Monkey' you can -l animals and then see 'Monkey'
+    //      And delte the animal. deleting non-existing entry
+    //      Throws an error.
 
     console.log('-------   Deleting...  -------');
     console.log('#-----------------------------#');
@@ -88,9 +155,10 @@ if (workingNote.valid() === true) {
       useUnifiedTopology: true }
     )
 
-    noteModel.deleteOne({ text: `${object.payload}`}, function (err) {
+    noteModel.deleteOne({ text: `${data.payload}`}, function (err) {
       if(err) console.log('Sorry, not found.');
       console.log('Gone!');
+      process.exit(1);
     });
 
 
@@ -103,4 +171,4 @@ if (workingNote.valid() === true) {
 
 }
 
-
+// close program:
